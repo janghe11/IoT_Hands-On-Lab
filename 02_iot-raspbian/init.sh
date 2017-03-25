7718ff52a3147f4cc8db761462d31846ae3cc44a
Taehee Jang <jang0913@gmail.com>
2017-03-25 16:45:12 +0900

    raspbian 실습 자습서 1차 완료

--- 02_iot-raspbian/init.sh
+++ 02_iot-raspbian/init.sh
@@ -2,7 +2,7 @@
 
 # Check if it has root permission
 if [[ "$UID" -ne "$ROOT_UID" ]] ; then
-	printf "죄송합니다! 관리자 권한으로 실행하여 주세요. 화면에 \"sudo iot-raspbian.sh\" 이라고 입력합니다.\n"
+	printf "죄송합니다! 관리자 권한으로 실행하여 주세요. 화면에 \"sudo sh init.sh\" 이라고 입력합니다.\n"
 	exit 1
 fi
 
@@ -13,6 +13,7 @@ apt update
 apt -y install vim
 apt -y install node
 apt -y install npm
+apt -y install python-dev
 apt -y install python-pip
 
 sed -i "s/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g" /boot/config.txt
@@ -24,9 +25,8 @@ npm install -g node-red-contrib-azure-iot-hub
 sleep 2s
 pip install python-twitter
 
+reboot
+
 #Mac OS SD 테스트
-change RGB rose gold
-#su pi -c "curl -L https://aka.ms/InstallAzureCli | bash"
-#su pi -c "exec -l $SHELL"
 
 #az iot device create --device-id grovepi --hub-name TaeheeIoTHub --resource-group Taehee_IoT-Test
